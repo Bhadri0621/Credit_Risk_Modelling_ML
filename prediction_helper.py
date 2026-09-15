c8701ff995c66a77cde61bcfd97be4d287c8133a
Bhadri0621 <kadiyala.bhadri@gmail.com>
2026-09-15 20:27:58 +0530

    Updated prediction_helper.py

--- prediction_helper.py
+++ prediction_helper.py
@@ -72,7 +72,7 @@ def calculate_credit_score(input_df,base_score=300,scale_length=600):
 
     rating=get_rating(credit_score[0])  
 
-    return default_probability.flatten()[0],int(credit_score),rating  
+    return default_probability.flatten()[0],int(credit_score[0]),rating  
 def predict(age, income, loan_amount, loan_tenure_months, avg_dpd,
                     delinquent_ratio, credit_utilization_ratio, num_open_accounts, residence_type,
                     loan_purpose, loan_type):
